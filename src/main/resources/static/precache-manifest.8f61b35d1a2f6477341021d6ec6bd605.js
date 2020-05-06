0268f63817f06a4cbb6e82ad1036e7194d69df24
Mittal Patel <mittal161195@gmail.com>
2020-05-05 22:52:11 -0400

    Changes made

---
+++ src/main/resources/static/precache-manifest.8f61b35d1a2f6477341021d6ec6bd605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56005f073cbb5e44858665ae3f8e786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34c5233f6feaef9dddab",
+    "url": "/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "34c5233f6feaef9dddab",
+    "url": "/static/js/2.33a0e111.chunk.js"
+  },
+  {
+    "revision": "44546482ab4964c96e0b609fce6539aa",
+    "url": "/static/js/2.33a0e111.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa8f524f3bf6a696638c",
+    "url": "/static/js/main.9a08a9bb.chunk.js"
+  },
+  {
+    "revision": "947f1077fc7d68bae6f9",
+    "url": "/static/js/runtime-main.7591f384.js"
+  }
+]);
